aceabf3c0597f15f83d44280195f6582007a748d
damien <yithanglee@gmail.com>
2026-08-29 11:18:44 +0800

    fix(migrations): use IF NOT EXISTS for AddOperatingHoursToOutlets and AddYoutubeUrlToSlides

--- priv/repo/migrations/20260811143700_add_operating_hours_to_outlets.exs
+++ priv/repo/migrations/20260811143700_add_operating_hours_to_outlets.exs
@@ -1,10 +1,13 @@
 defmodule BlogEngine.Repo.Migrations.AddOperatingHoursToOutlets do
   use Ecto.Migration
 
-  def change do
-    alter table(:outlets) do
-      add :operating_hours_start, :time
-      add :operating_hours_end, :time
-    end
+  def up do
+    execute("ALTER TABLE outlets ADD COLUMN IF NOT EXISTS operating_hours_start TIME;")
+    execute("ALTER TABLE outlets ADD COLUMN IF NOT EXISTS operating_hours_end TIME;")
+  end
+
+  def down do
+    execute("ALTER TABLE outlets DROP COLUMN IF EXISTS operating_hours_start;")
+    execute("ALTER TABLE outlets DROP COLUMN IF EXISTS operating_hours_end;")
   end
 end
--- priv/repo/migrations/20260829102450_add_youtube_url_to_slides.exs
+++ priv/repo/migrations/20260829102450_add_youtube_url_to_slides.exs
@@ -1,9 +1,11 @@
 defmodule BlogEngine.Repo.Migrations.AddYoutubeUrlToSlides do
   use Ecto.Migration
 
-  def change do
-    alter table("slides") do
-      add :youtube_url, :string
-    end
+  def up do
+    execute("ALTER TABLE slides ADD COLUMN IF NOT EXISTS youtube_url VARCHAR(255);")
+  end
+
+  def down do
+    execute("ALTER TABLE slides DROP COLUMN IF EXISTS youtube_url;")
   end
 end
